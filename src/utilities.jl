ecf8de8f35f51e4be9f20529ec98e806c84d4c55
Karel Mundnich <mundnich@usc.edu>
2019-01-27 15:44:47 -0800

    Added more utilities

--- src/utilities.jl
+++ src/utilities.jl
@@ -13,18 +13,22 @@ end
 
 function createtoydata(n1::Int64, n2::Int64)
     # Create toy data
-    x1, y1 = meshgrid(linspace(-5.0, 5.0, n1), linspace(1.0 ,6.0, n1))
+    x1, y1 = meshgrid(linspace(-5.0, 5.0, n1), linspace(1.0, 6.0, n1))
     D1 = [x1[:] y1[:]]
 
-    x2, y2 = meshgrid(linspace(-3.5,-1.5, n2), linspace(2.5 ,4.5, n2))
+    x2, y2 = meshgrid(linspace(-3.5,-1.5, n2), linspace(2.5, 4.5, n2))
     D2 = [x2[:] y2[:]]
 
-    x3, y3 = meshgrid(linspace( 1.5, 3.5, n2), linspace(2.5 ,4.5, n2))
+    x3, y3 = meshgrid(linspace( 1.5, 3.5, n2), linspace(2.5, 4.5, n2))
     D3 = [x3[:] y3[:]]
 
 
     return [D1; D2; D3]
 
+end
+
+function meshgrid(x, y)
+    return [i for j in y, i in x]
 end
 
 function unique_triplets(n::Int64)
@@ -82,6 +86,37 @@ function label(data::Array{Float64,2}; probability_success::Array{Float64,3}=one
 			end
 	    end
 	end
+
+    return triplets[1:counter,:]
+end
+
+function label_distances(D::Array{Int64,2}; probability_success::Array{Float64,3}=ones(size(D,1),size(D,1),size(D,1)))::Array{Int64,2}
+    n = size(D,1)
+    triplets = zeros(Int64, n*binomial(n-1, 2), 3)
+    counter = 0
+
+    for k = 1:n, j = 1:k-1, i = 1:n
+        if i != j && i != k
+
+            @inbounds mistake = probability_success[i,j,k] .<= 1 - rand()
+
+            if D[i,j] < D[i,k]
+                counter +=1
+                if !mistake
+                    @inbounds triplets[counter,:] = [i, j, k]
+                else
+                    @inbounds triplets[counter,:] = [i, k, j]
+                end
+            elseif D[i,j] > D[i,k]
+                counter += 1
+                if !mistake
+                    @inbounds triplets[counter,:] = [i, k, j]
+                else
+                    @inbounds triplets[counter,:] = [i, j, k]
+                end
+            end
+        end
+    end
 
     return triplets[1:counter,:]
 end
@@ -172,8 +207,8 @@ end
 function subset(triplets::Array{Int64,2}, fraction::Real)
         @assert !isempty(triplets) # Check for no triplets
 
-        no_triplets = size(triplets, 1)
-        amount = convert(Int64, floor(fraction * no_triplets))
+        n = maximum(triplets)
+        amount = floor(Int64, fraction * n * binomial(n-1, 2))
 
         triplets = triplets[shuffle(1:end),:]
 
@@ -199,6 +234,16 @@ function distances(X::Array{Float64,2}, no_objects::Int64)::Array{Float64,2}
 
     for j = 1:no_objects, i = j:no_objects
         @inbounds D[i,j] = norm(X[i,:] - X[j,:])
+    end
+
+    return D + D'
+end
+
+function distances(X::Array{Int64,1}; no_objects::Int64=length(X))::Array{Int64,2}
+    D = zeros(Int64, no_objects, no_objects)
+
+    for j = 1:no_objects, i = j:no_objects
+        @inbounds D[i,j] = abs(X[i] - X[j])
     end
 
     return D + D'
@@ -260,6 +305,17 @@ function flip_triplets!(triplets::Array{Int64,2}, amount::Int64)
 	end
 end
 
+function scale(data::Array{Int64,1}, X::Array{Float64,1})
+    # We solve the scaling problem by min || aX - data - b||^2,
+    # where (a,b) are the scale and offset parameters
+    @assert size(data) == size(X)
+
+    a, b = [X -ones(size(X))]\data
+
+    return a*X - b
+
+end
+
 function scale(data::Array{Float64,1}, X::Array{Float64,1}; MSE::Bool=false)
     # We solve the scaling problem by min || aX - data - b||^2,
     # where (a,b) are the scale and offset parameters
@@ -274,121 +330,12 @@ function scale(data::Array{Float64,1}, X::Array{Float64,1}; MSE::Bool=false)
     end
 end
 
-function procrustes(X::Array{Float64,2}, Y::Array{Float64,2}; doReflection=true)
-    #PROCRUSTES Procrustes Analysis
-    #   D = PROCRUSTES(X, Y) determines a linear transformation (translation,
-    #   reflection, orthogonal rotation, and scaling) of the points in the
-    #   matrix Y to best conform them to the points in the matrix X.  The
-    #   "goodness-of-fit" criterion is the sum of squared errors.  PROCRUSTES
-    #   returns the minimized value of this dissimilarity measure in D.  D is
-    #   standardized by a measure of the scale of X, given by
-    #
-    #      sum(sum((X - repmat(mean(X,1), size(X,1), 1)).^2, 1))
-    #
-    #   i.e., the sum of squared elements of a centered version of X.  However,
-    #   if X comprises repetitions of the same point, the sum of squared errors
-    #   is not standardized.
-    #
-    #   X and Y are assumed to have the same number of points (rows), and
-    #   PROCRUSTES matches the i'th point in Y to the i'th point in X.  Points
-    #   in Y can have smaller dimension (number of columns) than those in X.
-    #   In this case, PROCRUSTES adds columns of zeros to Y as necessary.
-    #
-    #   [D, Z] = PROCRUSTES(X, Y) also returns the transformed Y values.
-    #
-    #   Examples:
-    #
-    #      # Create some random points in two dimensions
-    #      n = 10;
-    #      X = normrnd(0, 1, [n 2]);
-    #
-    #      # Those same points, rotated, scaled, translated, plus some noise
-    #      S = [0.5 -sqrt(3)/2; sqrt(3)/2 0.5]; # rotate 60 degrees
-    #      Y = normrnd(0.5*X*S + 2, 0.05, n, 2);
-    #
-    #      # Conform Y to X, plot original X and Y, and transformed Y
-    #      [d, Z, tr] = procrustes(X,Y);
-    #      plot(X(:,1),X(:,2),'rx', Y(:,1),Y(:,2),'b.', Z(:,1),Z(:,2),'bx');
-    #
-    #   See also FACTORAN, CMDSCALE.
+function scale(data::Array{Float64,1}, X::Array{Float64,1}, t1::Int64, t2::Int64)
+    # We solve the scaling problem by min || aX - data - b||^2,
+    # where (a,b) are the scale and offset parameters
+    @assert size(data) == size(X)
 
-    #   References:
-    #     [1] Seber, G.A.F., Multivariate Observations, Wiley, New York, 1984.
-    #     [2] Gower, J.C. and Dijskterhuis, G.B., Procrustes Problems, Oxford
-    #         Statistical Science Series, Vol 30. Oxford University Press, 2004.
-    #     [3] Bulfinch, T., The Age of Fable; or, Stories of Gods and Heroes,
-    #         Sanborn, Carter, and Bazin, Boston, 1855.
+    a, b = [X[t1:t2] -ones(size(X[t1:t2]))]\data[t1:t2]
 
-    n, m   = size(X)
-    ny, my = size(Y)
-
-    if ny != n
-        error("procrustes: Input size mismatch")
-    elseif my > m
-        error("procrustes: Too many columns")
-    end
-
-    # Center at the origin.
-    muX = mean(X,1)
-    muY = mean(Y,1)
-    X0 = X - repmat(muX, n, 1)
-    Y0 = Y - repmat(muY, n, 1)
-
-    ssqX = sum(X0.^2,1)
-    ssqY = sum(Y0.^2,1)
-    constX = all(ssqX .<= abs.(eps(eltype(X))*n*muX).^2)
-    constY = all(ssqY .<= abs.(eps(eltype(X))*n*muY).^2)
-    ssqX = sum(ssqX)
-    ssqY = sum(ssqY)
-
-    if !constX && !constY
-        # The "centered" Frobenius norm.
-        normX = sqrt(ssqX) # == sqrt(trace(X0*X0'))
-        normY = sqrt(ssqY) # == sqrt(trace(Y0*Y0'))
-
-        # Scale to equal (unit) norm.
-        X0 = X0 / normX
-        Y0 = Y0 / normY
-
-        # Make sure they're in the same dimension space.
-        if my < m
-            Y0 = [Y0 zeros(Float64, n, m-my)]
-        end
-
-        # The optimum rotation matrix of Y.
-        A = X0' * Y0
-        L, D, M = svd(A)
-        T = M * L'
-        
-        haveReflection = det(T) < 0
-        if doReflection != haveReflection
-            # ... then either force a reflection, or undo one.
-            M[:,end] = -M[:,end]
-            D[end,end] = -D[end,end]
-            T = M * L'
-        end
-        
-        # The minimized unstandardized distance D(X0,b*Y0*T) is
-        # ||X0||^2 + b^2*||Y0||^2 - 2*b*trace(T*X0'*Y0)
-        traceTA = sum(D) # == trace(sqrtm(A'*A)) when doReflection is 'best'
-        
-        
-        # The optimum scaling of Y.
-        b = traceTA * normX / normY
-        
-        # The standardized distance between X and b*Y*T+c.
-        d = 1 - traceTA.^2
-
-        Z = normX*traceTA * Y0 * T + repmat(muX, n, 1)
-        
-    # The degenerate cases: X all the same, and Y all the same.
-    elseif constX
-        d = 0
-        Z = repmat(muX, n, 1)
-    else # !constX & constY
-        d = 1
-        Z = repmat(muX, n, 1)
-    end
-
-    return d, Z
+    return a,b
 end
