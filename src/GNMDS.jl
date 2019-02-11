9c3d9a217531680f7c86735f8eae646554c8f3f7
Karel Mundnich <mundnich@usc.edu>
2019-02-10 18:32:02 -0800

    Added loss functions: smooth_hinge, log, and exp (by creating new structs using metaprogramming)

--- src/GNMDS.jl
+++ src/GNMDS.jl
@@ -1,67 +1,75 @@
-struct GNMDS <: TripletEmbedding
-    loss::Symbol
-    triplets::Array{Int64,2}
-    dimensions::Int64
-    X::Embedding
-    no_triplets::Int64
-    no_items::Int64
+abstract type AbstractGNMDS <: TripletEmbedding end
 
-    function GNMDS(
-        loss::String,
-        triplets::Array{Int64,2},
-        dimensions::Int64,
-        X::Embedding)
+losses = [:hinge, :smooth_hinge, :log, :exp]
+
+# We use metaprogramming to generated different possible
+# structs (each for a different possible loss function).
+# Then, we can call the gradient for the type AbstractGNMDS,
+# but each gradient will be called using multiple dispatch.
+for symbol in losses
         
-        no_triplets::Int64 = size(triplets,1)
-        no_items::Int64 = maximum(triplets)
+    @eval struct $(Symbol(string("GNMDS_", symbol))) <: AbstractGNMDS
+            triplets::Array{Int64,2}
+            dimensions::Int64
+            X::Embedding
+            no_triplets::Int64
+            no_items::Int64
 
-        @check_embedding_conditions
-        
-        new(loss, triplets, dimensions, X, no_triplets, no_items)
-    end
+            function $(Symbol(string("GNMDS_", symbol)))(
+                loss::String,
+                triplets::Array{Int64,2},
+                dimensions::Int64,
+                X::Embedding)
+                
+                no_triplets::Int64 = size(triplets,1)
+                no_items::Int64 = maximum(triplets)
 
-    """
-    Constructor that initializes with a random (normal)
-    embedding.
-    """
-    function GNMDS(
-        loss::Symbol,
-        triplets::Array{Int64,2},
-        dimensions::Int64)
+                @check_embedding_conditions
+                
+                new(triplets, dimensions, X, no_triplets, no_items)
+            end
 
-        no_triplets::Int64 = size(triplets,1)
-        no_items::Int64 = maximum(triplets)
+            """
+            Constructor that initializes with a random (normal)
+            embedding.
+            """
+            function $(Symbol(string("GNMDS_", symbol)))(
+                triplets::Array{Int64,2},
+                dimensions::Int64)
 
-        if dimensions > 0
-            X = Embedding(0.0001*randn(maximum(triplets), dimensions))
-        else
-            throw(ArgumentError("Dimensions must be >= 1"))
+                no_triplets::Int64 = size(triplets,1)
+                no_items::Int64 = maximum(triplets)
+
+                if dimensions > 0
+                    X = Embedding(0.0001*randn(maximum(triplets), dimensions))
+                else
+                    throw(ArgumentError("Dimensions must be >= 1"))
+                end
+
+                @check_embedding_conditions
+
+                new(triplets, dimensions, X, no_triplets, no_items)
+            end
+
+            """
+            Constructor that takes an initial condition.
+            """
+            function $(Symbol(string("GNMDS_", symbol)))(
+                triplets::Array{Int64,2},
+                X::Embedding)
+
+                no_triplets::Int64 = size(triplets,1)
+                no_items::Int64 = maximum(triplets)
+                dimensions = size(X,2)
+
+                @check_embedding_conditions
+
+                new(triplets, dimensions, X, no_triplets, no_items)
+            end
         end
-
-        @check_embedding_conditions
-
-        new(loss, triplets, dimensions, X, no_triplets, no_items)
-    end
-
-    """
-    Constructor that takes an initial condition.
-    """
-    function GNMDS(
-        loss::Symbol,
-        triplets::Array{Int64,2},
-        X::Embedding)
-
-        no_triplets::Int64 = size(triplets,1)
-        no_items::Int64 = maximum(triplets)
-        dimensions = size(X,2)
-
-        @check_embedding_conditions
-
-        new(loss, triplets, dimensions, X, no_triplets, no_items)
-    end
 end
 
-function hinge_kernel(te::GNMDS, D::Array{Float64,2})
+function kernel(te::GNMDS_hinge, D::Array{Float64,2})
 
     slack = zeros(no_triplets(te))
     
@@ -72,10 +80,63 @@ function hinge_kernel(te::GNMDS, D::Array{Float64,2})
 
         @inbounds slack[t] = max(D[i,j] + 1 - D[i,k], 0)
     end
-    return slack
+    return Dict(:slack => slack)
 end
 
-function distances(te::GNMDS)
+function kernel(te::GNMDS_smooth_hinge, D::Array{Float64,2})
+    
+    weights = zeros(no_triplets(te))
+    slack = zeros(no_triplets(te))
+
+    for t in 1:no_triplets(te)
+        @inbounds i = triplets(te)[t,1]
+        @inbounds j = triplets(te)[t,2]
+        @inbounds k = triplets(te)[t,3]        
+        
+        @inbounds weights[t] = exp(1 + D[i,j] - D[i,k])
+        @inbounds slack[t] = log(1 + weights[t])
+        @inbounds weights[t] = weights[t] / (1 + weights[t])
+
+    end
+    return Dict(:slack => slack, :weights => weights)
+end
+
+
+function kernel(te::GNMDS_log, D::Array{Float64,2})
+    
+    weights = zeros(no_triplets(te))
+    slack = zeros(no_triplets(te))
+
+    for t in 1:no_triplets(te)
+        @inbounds i = triplets(te)[t,1]
+        @inbounds j = triplets(te)[t,2]
+        @inbounds k = triplets(te)[t,3]        
+        
+        @inbounds weights[t] = exp(D[i,j] - D[i,k])
+        @inbounds slack[t] = log(1 + weights[t])
+        @inbounds weights[t] = weights[t] / (1 + weights[t])
+
+    end
+    return Dict(:slack => slack, :weights => weights)
+end
+
+function kernel(te::GNMDS_exp, D::Array{Float64,2})
+    
+    slack = zeros(no_triplets(te))
+
+    for t in 1:no_triplets(te)
+        @inbounds i = triplets(te)[t,1]
+        @inbounds j = triplets(te)[t,2]
+        @inbounds k = triplets(te)[t,3]        
+        
+        # This operation is slow with negative values,
+        # and Yeppp takes vectors only...
+        @inbounds slack[t] = exp(D[i,j] - D[i,k])
+    end
+    return Dict(:slack => slack, :weights => slack)
+end
+
+function distances(te::AbstractGNMDS)
     sum_X = zeros(Float64, no_items(te), )
     D = zeros(Float64, no_items(te), no_items(te))
 
@@ -94,27 +155,53 @@ function distances(te::GNMDS)
     return D
 end
 
-function gradient(te::GNMDS)
+function gradient(te::AbstractGNMDS)
 
     D = distances(te)
-    slack = hinge_kernel(te, D)
-    C = sum(slack)
+    params = kernel(te, D)
+    C = sum(params[:slack])
 
     # We use single thread here since it is faster than
     # multithreading in Julia v1.0.3
     # We leave the multithreaded version of gradient_kernel
     # for the future
-    ∇C = gradient_kernel(te, slack)
+    ∇C = gradient_kernel(te, params)
+
 
     return C, ∇C
 
 end
 
-function gradient_kernel(te::GNMDS,
-                         slack::Array{Float64,1})
+function gradient_kernel!(te::AbstractGNMDS,
+                         violations::Array{Bool,1},
+                         ∇C::Array{Float64,2},
+                         triplets_range::UnitRange{Int64})
+    
+    for t in triplets_range
+        if violations[t]
+            @inbounds i = triplets(te)[t,1]
+            @inbounds j = triplets(te)[t,2]
+            @inbounds k = triplets(te)[t,3]
+
+            for d in 1:dimensions(te)
+                @inbounds dx_j = X(te)[i,d] - X(te)[j,d]
+                @inbounds dx_k = X(te)[i,d] - X(te)[k,d]
+                
+                @inbounds ∇C[i,d] +=   2 * (dx_j - dx_k)
+                @inbounds ∇C[j,d] += - 2 * dx_j
+                @inbounds ∇C[k,d] +=   2 * dx_k
+            end
+        end
+    end
+
+    return ∇C
+end
+
+function gradient_kernel(te::GNMDS_hinge,
+                         params::Dict{Symbol,Array{Float64,1}})
     
     violations = zeros(Bool, no_triplets(te))
-    @. violations = slack > 0
+    @. violations = params[:slack] > 0
     ∇C = zeros(Float64, no_items(te), dimensions(te))
 
     for t in eachindex(violations)
@@ -138,27 +225,34 @@ function gradient_kernel(te::GNMDS,
     return ∇C
 end
 
-function gradient_kernel!(te::GNMDS,
-                         violations::Array{Bool,1},
-                         ∇C::Array{Float64,2},
-                         triplets_range::UnitRange{Int64})
+for symbol in [:smooth_hinge, :log, :exp]
     
-    for t in triplets_range
-        if violations[t]
-            @inbounds i = triplets(te)[t,1]
-            @inbounds j = triplets(te)[t,2]
-            @inbounds k = triplets(te)[t,3]
+    @eval function gradient_kernel(te::$(Symbol(string("GNMDS_", symbol))),
+                             params::Dict{Symbol,Array{Float64,1}})
+        
+        violations = zeros(Bool, no_triplets(te))
+        @. violations = params[:slack] > 0
+        ∇C = zeros(Float64, no_items(te), dimensions(te))
 
-            for d in 1:dimensions(te)
-                @inbounds dx_j = X(te)[i,d] - X(te)[j,d]
-                @inbounds dx_k = X(te)[i,d] - X(te)[k,d]
-                
-                @inbounds ∇C[i,d] +=   2 * (dx_j - dx_k)
-                @inbounds ∇C[j,d] += - 2 * dx_j
-                @inbounds ∇C[k,d] +=   2 * dx_k
+        for t in eachindex(violations)
+            if violations[t]
+                # We obtain the indices for readability
+                @inbounds i = triplets(te)[t,1]
+                @inbounds j = triplets(te)[t,2]
+                @inbounds k = triplets(te)[t,3]
+                @inbounds w = params[:weights][t]
+
+                for d in 1:dimensions(te)
+                    @inbounds dx_j = 2 * w * (X(te)[i,d] - X(te)[j,d])
+                    @inbounds dx_k = 2 * w * (X(te)[i,d] - X(te)[k,d])
+
+                    @inbounds ∇C[i,d] +=   (dx_j - dx_k)
+                    @inbounds ∇C[j,d] += - dx_j
+                    @inbounds ∇C[k,d] +=   dx_k
+                end
             end
         end
-    end
 
-    return ∇C
+        return ∇C
+    end
 end
