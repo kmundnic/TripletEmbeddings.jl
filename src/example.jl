7fc30ea42f4aea9b7063e771d53df1706255ffec
Karel Mundnich <mundnich@usc.edu>
2019-02-12 11:48:16 -0800

    Usage examples for using with Julia v1.0

--- src/example.jl
+++ src/example.jl
@@ -1,11 +1,11 @@
 using Random
-# using Plots; pyplot()
+using Plots; plotlyjs()
 using BenchmarkTools
 include("Embeddings.jl")
 
 Random.seed!(4)
 
-# Load data and slice to have 1 sample/second
+# Load data dummy data
 data = Embeddings.load_data()
 
 # Generate triplets
@@ -14,14 +14,31 @@ triplets = Embeddings.label(data)
 # Calculate embedding from triplets
 dimensions = 1
 params = Dict{Symbol,Real}()
-params[:λ] = 0.0 # L2 regularization parameter
 params[:α] = 30 # Degrees of freedom of the t-Student
 
 te = Embeddings.tSTE(triplets, dimensions, params)
 
-# Compute embeddings
-@time bla, _ = Embeddings.compute(te; max_iter=50)
+# # Compute embeddings
+# @time violations = Embeddings.compute(te; max_iter=50)
 
-# X = Embeddings.scale(data, dropdims(X, dims=2))
-# plot(X)
-# plot!(data)
+# X = Embeddings.scale(data, dropdims(te.X.X, dims=2))
+# plot(data, label="Data")
+# plot!(X, label="tSTE")
+# # plot!(data)
+
+# dimensions = 1
+# params = Dict{Symbol,Real}()
+# params[:σ] = 1/sqrt(2) # Normal distribution variance
+
+# te = Embeddings.STE(triplets, dimensions, params)
+
+# # Compute embeddings
+# @time violations = Embeddings.compute(te; max_iter=50)
+
+# X = Embeddings.scale(data, dropdims(te.X.X, dims=2))
+# plot!(X, label="STE")
+
+# dimensions = 1
+# te = Embeddings.SmoothHingeGNMDS(triplets, dimensions)
+@time violations = Embeddings.compute(te; max_iter=50)
+# plot(Embeddings.X(te))
