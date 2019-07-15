09222c08d34e7a1f17550eb588edca6bd0fe8914
Karel Mundnich <mundnich@usc.edu>
2019-07-15 16:24:58 -0700

    Updated example

--- src/example.jl
+++ src/example.jl
@@ -19,7 +19,7 @@ params[:α] = 30 # Degrees of freedom of the t-Student
 te = Embeddings.tSTE(triplets, dimensions, params)
 
 # # Compute embeddings
-# @time violations = Embeddings.compute(te; max_iter=50)
+# @time violations = Embeddings.fit!(te; max_iter=50)
 
 # X = Embeddings.scale(data, dropdims(te.X.X, dims=2))
 # plot(data, label="Data")
@@ -33,12 +33,12 @@ te = Embeddings.tSTE(triplets, dimensions, params)
 # te = Embeddings.STE(triplets, dimensions, params)
 
 # # Compute embeddings
-# @time violations = Embeddings.compute(te; max_iter=50)
+# @time violations = Embeddings.fit!(te; max_iter=50)
 
 # X = Embeddings.scale(data, dropdims(te.X.X, dims=2))
 # plot!(X, label="STE")
 
 # dimensions = 1
 # te = Embeddings.SmoothHingeGNMDS(triplets, dimensions)
-@time violations = Embeddings.compute(te; max_iter=50)
+@time violations = Embeddings.fit!(te; max_iter=50)
 # plot(Embeddings.X(te))
